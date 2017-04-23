b831234a0ebd16566b288e140ce8b2ddbb8fe6f4
MF <notsure@compuserv.com>
2017-04-23 14:58:27 -0500

    updated pom file and switched out table for list

--- src/main/java/CubesGUI.java
+++ src/main/java/CubesGUI.java
@@ -1,16 +1,15 @@
 import javax.swing.*;
-import javax.swing.table.TableColumn;
 
 /**
  * Created by hl4350hb on 4/19/2017.
  */
 public class CubesGUI extends JFrame {
     private JPanel rootPanel;
-    private JTable botTable;
     private JButton addButton;
     private JButton updateButton;
     private JTextField botTextField;
     private JTextField timeTextField;
+    private JList botList;
 
     CubesGUI() {
         setContentPane(rootPanel);
@@ -18,9 +17,7 @@ public class CubesGUI extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
-//        TableColumn col = botTable.getTableHeader().getColumnModel().getColumn(0);
-        botTable.getTableHeader().getColumnModel().getColumn(0).setHeaderValue("Bot Name");
-        botTable.getTableHeader().getColumnModel().getColumn(1).setHeaderValue("Time");
+
     }
 }
 
